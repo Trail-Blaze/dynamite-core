955dc859c2d2f40ffcef6931cb12cef8aa7a46bb
Immanuel Garcia <34188635+AlexDev404@users.noreply.github.com>
2021-03-19 06:09:41 +0600

    Neonite Modules in modules folder needs rewriting
    Added unreleased alpha files, unsure if they work. Files in the /modules/ folder are still Neonite files, and need rewriting.

---
+++ app.js
@@ -0,0 +1,286 @@
+/*
+App.js - This script creates a static server, hosts files from the "public" directory and
+finds and empty port to listen on
+Copyright (C) 2021  Immanuel Garcia, Luke Harris, Sydney
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
+*/
+
+// Imports
+
+/* 
+VERSION LOG:
+1.0.0 - INITIAL RELEASE
+1.25.6 - ADD FEATURES AND BASE
+1.30.6 - NEONITE PATCH
+1.31.6 - DESTROY PARTS OF NEONITE CODE WITH ORIGINAL CODE 
+1.31.7 - EDIT ERRORS.JS PATH
+1.31.8 - REROUTE PATHS TO LIBS IN A MINIFIED WAY
+*/
+
+const express = require("express"); // Used to activate the NodeJs express application libraries
+const getPort = require("get-port"); // Used to listen for an empty random port
+const fs = require("fs"); // Used to access the immediate filesystem of an application.
+const { v4: uuidv4 } = require("uuid"); // Used to generate a unique random ID
+const errors = require("./lib/errors");
+const { ApiException } = errors;
+const console = require("console"); // Console Library
+const { exec } = require("child_process"); // Shell execution library
+const app = express(); // Create an express application
+const runfiles = "./modules"; // All core libraries and scripts required for Blaze to run
+const theme = require("./ThemePacks/CurrentTheme.js"); // Load theme
+const path = require("path"); // Used for file extension filter
+
+// Definitions
+
+const REQ_LOGGING = true; // Request Logging is set to false by default
+const version = "1.31.8";
+const cyear = 2021;
+const authors = "Immanuel Garcia, Luke Harris, Sydney";
+const windowTitle = "Blaze Server";
+
+// Imported and converted from a Python Project
+
+const bcolor = {
+  HEADER: "\033[95m",
+  OKBLUE: "\033[94m",
+  OKCYAN: "\033[96m",
+  OKGREEN: "\033[92m",
+  WARN: "\033[93m",
+  FAIL: "\033[91m",
+  END: "\033[0m",
+  BOLD: "\033[1m",
+  UNDERLINE: "\033[4m",
+};
+
+// A sleep function I found somewhere just in case
+
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+// Main application starts here!
+console.log("\n");
+exec(`title ${windowTitle} version ${version}`); // Sets the window title to Blaze Server
+
+// uuidv4(); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a'
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
+
+// https://www.w3schools.com/js/js_strict.asp
+("use strict"); // Prevents program from using uninitialized and undeclared variables
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("etag", false);
+
+if (REQ_LOGGING) {
+  // https://expressjs.com/en/guide/using-middleware.html#middleware.application
+
+  app.use((req, res, next) => {
+    // res (response variable) is never used in this scope
+    console.log(`${bcolor.OKGREEN}[GET]${bcolor.END}`, req.url); // Logs the response URL to the console
+    next();
+  });
+}
+// https://expressjs.com/en/starter/static-files.html
+
+app.use("/", express.static("public")); // Set serverdir to rootdir and create an HTTP server that serves static files
+
+/* app.use((req, res, next) => {
+  // req and res are never used in this scope
+  next(new ApiException(errors.com.epicgames.common.not_found)); // IDK
+});*/
+
+// Returns files with a certain file extension
+// https://stackoverflow.com/a/44200655/10976415
+
+let dirPath = path.resolve(runfiles); // path to your directory goes here
+let filesList;
+
+fs.readdir(dirPath, function (err, files) {
+  if (err) {
+    console.log("not found");
+  } else {
+    filesList = files.filter(function (e) {
+      return path.extname(e).toLowerCase() === ".js";
+    });
+    //  console.log(filesList);
+  }
+});
+
+init(); // Run init function
+
+async function init() {
+  await sleep(500); // Display main title for a bit then go onto the main stuffs
+
+  // https://github.com/sindresorhus/get-port/
+  // https://www.npmjs.com/package/get-port
+
+  const port = await getPort({ port: [80, 5595, 8080] }); // Dynamically fetches a random getPort
+  app.listen(port, () => {
+    console.clear();
+    exec(`title ${windowTitle} is listening on localhost port ${port}`); // Switch title to Blaze Server is listening on port {port}.
+
+    // Saves Port Number To File
+    // https://www.w3schools.com/nodejs/nodejs_filesystem.asp
+
+    start(); // Display Disclaimer and start Blaze
+
+    const filename_log = "port";
+
+    // Creates File if Not Found
+    // https://flaviocopes.com/how-to-check-if-file-exists-node/
+
+    fs.access(filename_log, fs.F_OK, (err) => {
+      if (err) {
+        // console.error(err); // For debugging purposes
+
+        console.log(
+          `${bcolor.OKBLUE}[INFO]${bcolor.END}`,
+          `File ${filename_log} Not Found! Is this a first-time run?`
+        );
+        console.log(
+          `${bcolor.OKBLUE}[INFO]${bcolor.END}`,
+          `File ${filename_log} Creating one...`
+        );
+
+        createPortfile();
+
+        return 0;
+      } // Deletes File if Found
+      fs.unlink(`${filename_log}`, function (err) {
+        if (err) throw err;
+        console.log(
+          `${bcolor.OKBLUE}[INFO]${bcolor.END}`,
+          `File ${filename_log} found. Deleting...\n`
+        );
+      });
+
+      createPortfile();
+
+      function createPortfile() {
+        // Recreates File With Correct Port Number
+
+        fs.writeFile(`${filename_log}`, `${port}`, function (err) {
+          if (err) throw err;
+          console.log(
+            `${bcolor.OKBLUE}[INFO]${bcolor.END}`,
+            `Saved Port Number to ${filename_log}\n`
+          );
+        });
+      }
+    });
+
+    // Check for manager directory
+
+    /* fs.access("./managers", function(error) {
+  if (error) {
+    console.log("Directory does not exist.")
+  } else {
+    console.log("Directory exists.")
+  }
+})*/
+
+    // PATCH 1.30.6! SWITCH TO NEONITE LIB!
+    function start() {
+      fs.access(runfiles, async function (err) {
+        if (err) {
+          // Stage 1 Fails
+
+          exec(`title ${windowTitle} has failed to load!`); // Switch title to "Blaze Server has failed to load!".
+
+          console.error(
+            `${bcolor.FAIL}Core Directory Does Not Exist. Blaze Cannot Continue to Launch.${bcolor.END}`
+          );
+          console.log("\n");
+          console.log(
+            `${bcolor.HEADER}Blaze has ${bcolor.FAIL}FAILED${bcolor.END}${bcolor.HEADER} to launch!${bcolor.END}`
+          );
+          console.log("\n");
+          console.error(`${bcolor.FAIL}THE FULL ERROR IS DISPLAYED BELOW:`);
+          console.error(err);
+          console.log(`${bcolor.HEADER}To exit, hit any key.${bcolor.END}\n`);
+          process.exit();
+        } else {
+          // Stage 1 Passes
+          console.log("\n");
+          console.log("Core directory exists. Loading required files...\n");
+
+          for (let range = 0; range < filesList.length; range++) {
+            require(`${runfiles}/${filesList[range]}`)(app, port);
+            console.log(
+              `${bcolor.OKCYAN}[OK] Importing: ${runfiles}/${filesList[range]}${bcolor.END}`
+            );
+          }
+          console.log("\n");
+
+          // Replaced shit code with better code.
+          /*  fs.readdirSync(`${__dirname}/managers`).forEach(route => { //for the managers folder
+            require(`${__dirname}/managers/${route}`)(app, 5595); //port number = 5595 
+          })*/
+
+          // NEONITE STARTS
+
+          app.use((next) => {
+            next(new ApiException(errors.com.epicgames.common.not_found));
+          });
+
+          app.use((err, req, res) => {
+            let error = null;
+
+            if (err instanceof ApiException) {
+              error = err;
+            } else {
+              const trackingId =
+                req.headers["x-epic-correlation-id"] || uuidv4();
+              error = new ApiException(
+                errors.com.epicgames.common.server_error
+              ).with(trackingId);
+              console.error(trackingId, err);
+            }
+
+            error.apply(res);
+          });
+          // Neonite ENDS
+          await sleep(2000);
+
+          console.clear();
+          console.log("\n");
+          console.log(`< Theoretical Ascii logo goes here >`);
+          console.log("\n");
+          console.log(
+            `${bcolor.OKGREEN}Blaze version ${version}, Copyright (C) ${cyear} ${authors}\nBlaze comes with ABSOLUTELY NO WARRANTY.\n\nThis is free software, and you are welcome to redistribute it under certain conditions.\nFor more information, please refer to the bundled LICENSE file.${bcolor.END}`
+          );
+          console.log("\n");
+          console.log(
+            `${bcolor.HEADER}Blaze has successfully initialized and is listening on port ${port}.${bcolor.END}`
+          );
+          console.log(
+            `${bcolor.HEADER}To exit, hit CTRL + C at any time.${bcolor.END}\n`
+          );
+        }
+      });
+    }
+    // Run Color Tests
+    /*
+    
+    console.log(`${bcolor.HEADER}HEADER${bcolor.END}`);
+    console.log(`${bcolor.OKBLUE}OKBLUE${bcolor.END}`);
+    console.log(`${bcolor.OKCYAN}OKCYAN${bcolor.END}`);
+    console.log(`${bcolor.OKGREEN}OKGREEN${bcolor.END}`);
+    console.log(`${bcolor.WARN}WARN${bcolor.END}`);
+    console.log(`${bcolor.FAIL}FAIL${bcolor.END}`);
+    console.log(`${bcolor.BOLD}BOLD${bcolor.END}`);
+    console.log(`${bcolor.UNDERLINE}UNDERLINE${bcolor.END}`);
+    */
+  });
+}
+module.exports = app;
---
+++ modules/oauth.js
@@ -0,0 +1,116 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const Profile = require("../lib/profile");
+
+module.exports = (app) => {
+	//token
+	app.post('/account/api/oauth/token', async (req, res) => {
+		var displayName = "";
+		var accountId = "";
+	
+                if (req.body.username) displayName = req.body.username.split("@")[0]
+		else if (req.body.email) displayName = req.body.email.split("@")[0]
+		else displayName = `InvalidUser${Math.random().toString().substring(15)}`
+		accountId = displayName.replace(/ /g, '_');
+		if (!accountId.startsWith("InvalidUser")) {
+			var profileId = "athena";
+			var profileData = Profile.readProfile(accountId, profileId);
+
+			if (!profileData) {
+				profileData = Profile.readProfileTemplate(profileId);
+
+				if (!profileData) {
+					throw new ApiException(errors.com.epicgames.modules.profiles.operation_forbidden).with(profileId);
+				}
+
+				profileData.created = profileData.updated = new Date().toISOString();
+				profileData['_id'] = accountId;
+				profileData.accountId = accountId;
+				//await Profile.updatedCos(profileData);
+
+				try {
+					fs.mkdirSync(`./config/${accountId}/profiles`, { recursive: true });
+					Profile.saveProfile(accountId, profileId, profileData);
+				} catch (e) {
+					console.log("Failed creating profile");
+					throw e;
+				}
+			}
+		}
+
+		res.json({
+			access_token: crypto.randomBytes(15).toString("hex"),
+			expires_in: 28800,
+			expires_at: "9999-12-31T23:59:59.999Z",
+			token_type: "bearer",
+			account_id: accountId,
+			client_id: "ec684b8c687f479fadea3cb2ad83f5c6",
+			internal_client: true,
+			client_service: "fortnite",
+			displayName: displayName,
+			app: "fortnite",
+			in_app_id: accountId,
+			device_id: "5dcab5dbe86a7344b061ba57cdb33c4f"
+		})
+	});
+
+	//verify
+	app.get('/account/api/oauth/verify', (req, res) => {
+		res.json({
+			access_token: req.headers.authorization.replace("bearer ", ""),
+			expires_in: 28800,
+			expires_at: "9999-12-31T23:59:59.999Z",
+			token_type: "bearer",
+			refresh_token: "cd581d37b0434726a37b0268bb99206c",
+			refresh_expires: 115200,
+			refresh_expires_at: "9999-12-31T23:59:59.999Z",
+			account_id: "ninja",
+			client_id: "3446cd72694c4a4485d81b77adbb2141",
+			internal_client: true,
+			client_service: "fortnite",
+			displayName: req.h,
+			app: "fortnite",
+			in_app_id: "ninja",
+			device_id: "164fb25bb44e42c5a027977d0d5da800"
+		})
+	});
+
+	//kill token
+	app.delete('/account/api/oauth/sessions/kill', (req, res) => {
+		res.status(204).end();
+	});
+
+	app.delete('/account/api/oauth/sessions/kill/:token', (req, res) => {
+		delete global[req.params.token]
+		res.status(204).end();
+	});
+
+	//account info
+	app.get('/account/api/public/account/:accountId', (req, res) => {
+		var displayname = `${req.params.accountId}`
+		if (displayname.endsWith('=')) displayname = Buffer.from(req.params.accountId, 'base64').toString();
+		res.json({
+			id: req.params.accountId,
+			displayName: displayname,
+			externalAuths: {}
+		})
+	});
+	//http://localhost:5595/account/api/public/account?accountId=NeoniteBot10&accountId=NeoniteBot11&accountId=NeoniteBot12&accountId=NeoniteBot13&accountId=NeoniteBot14&accountId=NeoniteBot15
+
+	app.get('/account/api/public/account/', (req, res) => {
+			res.json([{
+				id: req.query.accountId,
+				displayName: req.query.accountId.replace(/_/g, ' '),
+				externalAuths: {}
+			}])
+	});
+
+	// device auth
+	app.get('/account/api/public/account/:accountId/deviceAuth', (req, res) => res.json([]));
+
+	app.post('/account/api/public/account/:accountId/deviceAuth', (req, res) => res.json({
+		accountId: req.params.accountId,
+		deviceId: "aabbccddeeff11223344556677889900",
+		secret: "aabbccddeeff11223344556677889900"
+	}));
+}
