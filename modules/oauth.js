6084b9a6ee7af4a1aa4295f87546131879747d2d
zinx-YT <63958372+zinx-YT@users.noreply.github.com>
2021-04-28 19:48:56 +0000

    Update oauth.js

--- modules/oauth.js
+++ modules/oauth.js
@@ -51,12 +51,12 @@ module.exports = (app) => {
       expires_in: 1000000000,
       expires_at: "9999-12-31T23:59:59.999Z",
       token_type: "bearer",
-      account_id: userName + "BlazeUser", //amazing id
+      account_id: userName, //amazing id
       client_id: "ec684b8c687f479fadea3cb2ad83f5c6",
       internal_client: true,
       displayName: userName,
       app: "fortnite",
-      in_app_id: userName + "BlazeUser", //amazing id
+      in_app_id: userName, //amazing id
       device_id: "5dcab5dbe86a7344b061ba57cdb33c4f",
     });
   });
