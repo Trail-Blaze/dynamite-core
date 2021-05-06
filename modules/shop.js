1fb725e3fcb43099f1b604478cb28d8a283f0aa4
Immanuel Garcia <34188635+AlexDev404@users.noreply.github.com>
2021-05-06 14:31:23 -0600

    Update shop.js

--- modules/shop.js
+++ modules/shop.js
@@ -1,5 +1,13 @@
 module.exports = (app) => {
 	app.get('/fortnite/api/storefront/v2/catalog', function (req, res) {
-		res.json(require("../lib/shop.json"));
+    axios
+      .get("https://api.nitestats.com/v1/epic/store")
+      .then((response) => {
+        res.json(response.data);
+      })
+      .catch((e) => {
+        res.json(require("../lib/shop.json"));
+      });
+  });	
 	});
 }
