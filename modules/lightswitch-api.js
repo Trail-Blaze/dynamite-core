c44300fad74cf3a3ede0a99c21497ed94e86b16f
Immanuel Garcia <34188635+AlexDev404@users.noreply.github.com>
2021-05-07 15:35:18 -0600

    Create lightswitch-api.js

---
+++ modules/lightswitch-api.js
@@ -0,0 +1,13 @@
+  //check server status
+  app.get("/lightswitch/api/service/bulk/status", (req, res) => {
+    res.json([
+      {
+        serviceInstanceId: "fortnite",
+        status: "UP",
+        message: "200 OK",
+        maintenanceUri: null,
+        allowedActions: ["PLAY", "DOWNLOAD"],
+        banned: false,
+      },
+    ]);
+  });
