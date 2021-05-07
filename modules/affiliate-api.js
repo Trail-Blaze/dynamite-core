6c8eb657023e48dbe2085b88d096d187daed5a6c
Immanuel Garcia <34188635+AlexDev404@users.noreply.github.com>
2021-05-07 15:46:59 -0600

    Create affiliate-api.js

---
+++ modules/affiliate-api.js
@@ -0,0 +1,13 @@
+  //affiliate api
+  app.get(
+    "/affiliate/api/public/affiliates/slug/:affiliateName",
+    (req, res) => {
+      res.json({
+        id: "aabbccddeeff11223344556677889900",
+        slug: req.params.affiliateName,
+        displayName: req.params.affiliateName,
+        status: "ACTIVE",
+        verified: true,
+      });
+    }
+  );
