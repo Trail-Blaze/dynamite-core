2d1d3a0fc5356d5386c87ad391d7fe9354b8e029
Immanuel Garcia <34188635+AlexDev404@users.noreply.github.com>
2021-05-07 15:35:03 -0600

    Remove Lightswitch

--- modules/api.js
+++ modules/api.js
@@ -11,20 +11,6 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, "../lib/favicon.ico"))
   });
   
-  //check server status
-  app.get("/lightswitch/api/service/bulk/status", (req, res) => {
-    res.json([
-      {
-        serviceInstanceId: "fortnite",
-        status: "UP",
-        message: "200 OK",
-        maintenanceUri: null,
-        allowedActions: ["PLAY", "DOWNLOAD"],
-        banned: false,
-      },
-    ]);
-  });
-
   //version check
   app.get("/fortnite/api/v2/versioncheck/:version", (req, res) => {
     res.json({ type: "NO_UPDATE" });
