78aad6506918e7c0bf1bdf6c6a13dac54d63d39c
zinx <63958372+zinx-YT@users.noreply.github.com>
2022-01-30 01:14:46 +0000

    Update region.js

--- modules/region.js
+++ modules/region.js
@@ -1,5 +1,5 @@
 /*
-account-api.js
+region.js
 Copyright (C) 2021  Immanuel Garcia
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
